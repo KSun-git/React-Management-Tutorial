d60c74c54c6dd12c46043297f2a9cf490a77affa
ksy03070 <ksy03070@gmail.com>
2020-10-07 10:35:15 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component{
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
+}
+
+{/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, React!
         </p>
         <a
           className="App-link"
@@ -22,5 +34,6 @@ function App() {
     </div>
   );
 }
+*/}
 
 export default App;
