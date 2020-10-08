a94ae2f176f9379c95e3eac4a79c9c4686a95bea
ksy03070 <ksy03070@gmail.com>
2020-10-08 14:38:54 +0900

    10,11강 - Create Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,27 @@ const port = process.env.Port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req,res) => {
     //고객목록 리스트 출력
-    res.send([
-        {
-          'id' : 'kch',
-          'image' : 'https://placeimg.com/64/64/1',
-          'name' : '김철수',
-          'birthday' : '961222',
-          'gender' : '남자',
-          'job' : '프로그래머'
-        },
-        {
-          'id' : 'kyh',
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : '김영희',
-          'birthday' : '960305',
-          'gender' : '여자',
-          'job' : '프로그래머'
-        },
-        {
-            'id' : 'hgd',
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '홍길동',
-            'birthday' : '960701',
-            'gender' : '남자',
-            'job' : '프로그래머'
-          }
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
